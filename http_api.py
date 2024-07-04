af9792b58c83db935618168b4405a33dac549a36
Mamoutou <mamoutou.diarra@hivenet.com>
2024-07-04 14:05:04 +0200

    set chunk size to 1024, k to 4 and overlap to 100 v2.7

--- http_api.py
+++ http_api.py
@@ -76,7 +76,7 @@ def update_from_hiveDisk():
     logger.info(f"Loading HiveDisk documents from {config.SOURCE_DIRECTORY}")
     documents = load_documents(config.SOURCE_DIRECTORY)
     text_documents, python_documents = split_documents(documents)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=100)
     python_splitter = RecursiveCharacterTextSplitter.from_language(
         language=Language.PYTHON, chunk_size=512, chunk_overlap=100
     )
