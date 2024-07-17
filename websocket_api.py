e7672f8148e34be687a6742e42996faa5f085ba2
Mamoutou <mamoutou.diarra@hivenet.com>
2024-07-17 16:20:07 +0200

    add query v2.6

--- websocket_api.py
+++ websocket_api.py
@@ -203,7 +203,7 @@ def ws_api_generate(ws):
             retrieved_docs = retriever.get_relevant_documents(message)
             GLOBAL_REFERENCES = [doc.metadata["source"] for doc in retrieved_docs]
             context = " ".join([doc.page_content for doc in retrieved_docs])
-            qa.run({"context": context, "question": message})
+            qa.run({"context": context, "query": message})
             
 
         t = threading.Thread(target=run_enhanced_rqa, args=(UserInput,))
