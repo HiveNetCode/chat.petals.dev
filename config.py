3f4973609e7cafe6cf774e8dc986ed898cbc202a
Mamoutou <mamoutou.diarra@hivenet.com>
2024-08-13 19:37:07 +0200

    set chunk to 1000 k=4 overlap=200 and remove mistral v1.9

--- config.py
+++ config.py
@@ -15,8 +15,7 @@ default_chat_config = ModelChatConfig(
 
 MODEL_FAMILIES = {
     
-    
-    "ALL": [
+    "Llama 2": [
         ModelConfig(
             ModelBackendConfig(repository="meta-llama/Llama-2-70b-chat-hf"),
             ModelFrontendConfig(
@@ -26,17 +25,8 @@ MODEL_FAMILIES = {
             ),
             default_chat_config,
         ),
-        ModelConfig(
-            ModelBackendConfig(repository="mistralai/Mixtral-8x7B-Instruct-v0.1"),
-            ModelFrontendConfig(
-                name="Mixtral 8x7B",
-                model_card="https://huggingface.co/mistralai/Mixtral-8x7B-Instruct-v0.1",
-                license="https://huggingface.co/mistralai/Mixtral-8x7B-Instruct-v0.1/tree/main",
-            ),
-            default_chat_config,
-        ),
     ],
-     
+      
 }
 MIXTRAL_MODEL_NAME = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 # Define RAG Retrieval Settings
